42aff08c83f16b99363b371d023bf9f24de0670b
SAEMC <saemc27@gmail.com>
2022-06-24 20:41:05 +0900

    Create & Move

---
+++ AbstractDataType/1_stack.ts
@@ -0,0 +1,49 @@
+export class Stack {
+  items: Array<any> = [];
+
+  isEmpty(): boolean {
+    return !Boolean(this.items.length);
+  }
+
+  push(value: any): void {
+    this.items.push(value);
+  }
+
+  pop(): any | void {
+    const value = this.items.pop();
+
+    if (value) {
+      return value;
+    } else {
+      console.log("Stack is empty.");
+    }
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  peek(): Array<any> | void {
+    if (this.items) {
+      return this.items[this.items.length - 1];
+    } else {
+      console.log("Stack is empty.");
+    }
+  }
+}
+
+const stack = new Stack();
+
+console.log(`스택이 비었나요? ${stack.isEmpty()}`);
+console.log(`스택에 숫자 0~9를 추가합니다.`);
+
+for (let i = 0; i < 10; i++) {
+  stack.push(i);
+}
+
+console.log(`스택 크기: ${stack.size()}`);
+console.log(`peek: ${stack.peek()}`);
+console.log(`pop: ${stack.pop()}`);
+console.log(`peek: ${stack.peek()}`);
+console.log(`스택이 비었나요? ${stack.isEmpty()}`);
+console.log(stack.items);
